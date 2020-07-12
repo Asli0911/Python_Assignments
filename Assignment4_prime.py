3452e59a7fe3139a5165fe96ae4aa5b40b37249f
Asli0911 <aslikonusbm@gmail.com>
2020-07-12 18:57:00 +0200

    Assignment 4 is done.

--- Assignment4_prime.py
+++ Assignment4_prime.py
@@ -1,16 +1,17 @@
 number=int(input("Please enter a number:"))
 
-i=2
+i=1
 divide=[]
 
 while i**2<=number:
     if number%i==0:
         divide.append(i)
     i+=1
-
-if number==1:
+if number==1 or number==0:
     print(number, "is not a prime number.")
-elif len(divide)>=1:
+ 
+elif len(divide)>=2:
     print(number, "is not a prime number.")
+    
 else:
     print(number, "is a prime number.")
