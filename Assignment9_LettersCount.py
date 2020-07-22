d102c6d41da455237ceca66733f5c1f0c0da0ca0
Asli0911 <aslikonusbm@gmail.com>
2020-07-22 08:38:30 +0200

    Assignment9 is done.

--- Assignment9_LettersCount.py
+++ Assignment9_LettersCount.py
@@ -1,7 +1,9 @@
-sentence=input("Please write a sentence:")
+sentence=input("Please write a sentence:").lower()
 set1=set(sentence)
 answer={}
 for i in set1:
     answer[i]=sentence.count(i)
 print(answer)    
+
+
 
