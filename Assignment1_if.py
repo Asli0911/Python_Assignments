3c817048cc07498e3b03741de788e146b58ed7a7
Asli0911 <aslikonusbm@gmail.com>
2020-07-26 16:50:02 +0200

    Assignment1 is updated

--- Assignment1_if.py
+++ Assignment1_if.py
@@ -1,6 +1,6 @@
 name=input("Please enter your name:").capitalize()
 
 if name=="Asli":
-    print("Hello, Asli! Your password is:313*")
+    print("Hello, Asli! Your password is:313_a")
 else:
     print("Hello, {}! See you later".format(name))
