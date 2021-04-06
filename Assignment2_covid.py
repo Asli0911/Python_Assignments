d1e329b1077e3caf5fae8c2352bfdfa7aa19c0ae
Asli Konus <67109567+Asli0911@users.noreply.github.com>
2021-04-06 10:57:13 +0200

    Update Assignment2_covid.py

--- Assignment2_covid.py
+++ Assignment2_covid.py
@@ -1,7 +1,5 @@
 age= input ("Are you a cigarette addict older than 75 years old?").lower() 
 chronic= input ("Do you have a severe chronic disease?").lower() 
 immune= input ("Is your immune system too weak?").lower() 
-
 risk= age=="yes" or chronic=="yes" or immune=="yes"
-
 print ("You are in risky group"*risk + "You are not in risky group"* (not risk))   
